26ecd272c7508bf4775ec8c8e26626dd03a97d04
Kåre Sørensen <kaare@voicearchive.com>
2013-06-24 21:34:51 +0200

    added some ui test code

--- ui/beautiful.py
+++ ui/beautiful.py
@@ -15,5 +15,32 @@ class MyFrame(wx.Frame):
     for row in obj.list():
       res += ("" + str(row) + "\n")
 
-    self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE).AppendText(res)
+    self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+    self.CreateStatusBar()
+    filemenu= wx.Menu()
+    menuAbout = filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+    filemenu.AppendSeparator()
+    menuExit = filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+    menuBar = wx.MenuBar()
+    menuBar.Append(filemenu,"&File")
+    self.SetMenuBar(menuBar)
+    self.sizerH = wx.BoxSizer(wx.HORIZONTAL)
+    clearButton = wx.Button(self, -1, "Execute query")
+    self.sizerH.Add(clearButton, 1, wx.EXPAND)
+    self.sizer = wx.BoxSizer(wx.VERTICAL)
+    self.sizer.Add(self.control, 1, wx.EXPAND)
+    self.sizer.Add(self.sizerH, 0, wx.EXPAND)
+    self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+    self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+    self.SetSizer(self.sizer)
+    self.SetAutoLayout(1)
+    self.sizer.Fit(self)
     self.Show(True)
+
+  def OnAbout(self,e):
+    dlg = wx.MessageDialog( self, "A small text editor", "About Sample Editor", wx.OK)
+    dlg.ShowModal() # Show it
+    dlg.Destroy() # finally destroy it when finished.
+    
+  def OnExit(self,e):
+    self.Close(True)  # Close the frame.
